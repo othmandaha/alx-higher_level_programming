f1eecc44e047c028fb7fa499c5e29ca93ab62202
othmandaha <dahaothman02@gmail.com>
2024-01-02 22:49:00 +0100

    write sys call

--- 0x01-python-if_else_loops_functions/100-write.py
+++ 0x01-python-if_else_loops_functions/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-data = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-newline = "\n"
-sys.stderr.write(data + newline)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
