e5bb97a5de063daa081aac44f20f4c3398b36431
othmandaha <dahaothman02@gmail.com>
2024-01-04 16:28:37 +0100

    cmd arguments retrieval

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     from sys import argv
 
     length = len(argv) - 1
-    print("{} arguments:".format(length))
-    i = 1
+    if length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
 
+    i = 1
     while i <= length:
         print("{}: {}".format(i, argv[i]))
         i += 1
