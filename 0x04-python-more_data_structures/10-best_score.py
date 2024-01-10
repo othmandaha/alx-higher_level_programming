73a4035bc90af31aee7d8a9cde4969ee80f0da55
othmandaha <dahaothman02@gmail.com>
2024-01-10 20:44:14 +0100

    best score in dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
         best_score = 0
         name = None
         for keys, values in a_dictionary.items():
-            if value > best_score:
+            if values > best_score:
                 name = keys
         return name
