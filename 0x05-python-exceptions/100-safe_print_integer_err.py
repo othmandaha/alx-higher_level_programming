0dce14b3a58ac47b2263e9d6f2ca816344870947
othmandaha <dahaothman02@gmail.com>
2024-01-22 17:53:10 +0100

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+        for i in range(0, len(my_list_1):
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
