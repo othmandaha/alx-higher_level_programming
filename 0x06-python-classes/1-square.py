f0fb9996bffbbc9be728761b3122064116e1ba47
othmandaha <dahaothman02@gmail.com>
2024-01-23 17:49:36 +0100

    edited

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,8 +8,4 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new square"""
 
-        if not isinstance(size, int):
-            raise TypeError("Only Numbers are accepted")
-        elif size <= 0:
-            raise ValueError("Inappropriate argument value")
         self.__size = size
