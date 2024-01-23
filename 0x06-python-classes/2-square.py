4b8dd08a26fe60779c4bc6c28325cd2a919ab91d
othmandaha <dahaothman02@gmail.com>
2024-01-23 18:04:38 +0100

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """ a square blueprint """
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+        size (int): the size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
