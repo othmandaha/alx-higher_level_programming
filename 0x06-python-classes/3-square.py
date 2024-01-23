a91b8d5bf8b2df98a43c1de9a1ffc427e2651007
othmandaha <dahaothman02@gmail.com>
2024-01-23 18:26:19 +0100

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""The square blueprint"""
+
+
+class Square:
+    """The square class"""
+
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+            size (int): the size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Computes the ares of square (self)
+
+        Returns:
+            float: the area of the square
+        """
+        return self.__size ** 2
