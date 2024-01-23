b40196840b8512999c87ea693b1256010b45c7b9
othmandaha <dahaothman02@gmail.com>
2024-01-23 19:15:30 +0100

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""The square blueprint"""
+
+
+class Square:
+    """The square class"""
+
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+            size (int): the size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Computes the ares of square (self)
+
+        Returns:
+            float: the area of the square
+        """
+        return self.__size ** 2
