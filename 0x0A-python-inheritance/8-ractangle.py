f2dc342ca22d54c6048623b8bb8e26219f505fe6
othmandaha <dahaothman02@gmail.com>
2024-02-05 23:08:03 +0100

    create a rectangle

---
+++ 0x0A-python-inheritance/8-ractangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyton3
+""" a rectangle blueprint """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """the Rectangle class"""
+
+    def __init__(self, width, height):
+        """initialise a new rectangle.
+
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height fo the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
