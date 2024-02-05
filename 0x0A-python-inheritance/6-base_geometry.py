026ec979f42ab2bb8dd1d9f98f7316c3d7eb11c6
othmandaha <dahaothman02@gmail.com>
2024-02-05 21:43:50 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a base geometry BluePrint """
+
+
+class BaseGeometry:
+    """ the Base geometry class """
+
+    def are(self):
+        """ are calculator """
+        raise Exception("area() is not implemented")
