0196b92ff40ec368a8ef6896353f5847c55f7a87
othmandaha <dahaothman02@gmail.com>
2024-02-05 19:17:20 +0100

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,13 +2,8 @@
 """ A list class """
 
 
-class Mylist(List):
+class Mylist(list):
     """ a child class of list """
-
-    def __init__(self):
-        """ initialize the child class """
-        super().__init__()
-        """ initialize the parent class"""
 
     def print_sorted(self):
         """ print sorted """
