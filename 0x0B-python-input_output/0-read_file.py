a891027d31968675fe95c366dbfb9e955f2760ad
othmandaha <dahaothman02@gmail.com>
2024-02-06 13:17:38 +0100

    reading file function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""File Reading."""
+
+
+def read_file(filename=""):
+    """a function for reading from a file.
+
+    Args:
+    filename (str): the file name.
+    """
+    with open(filename, encoding="uft-8") as file:
+        print(file.read(), end='')
