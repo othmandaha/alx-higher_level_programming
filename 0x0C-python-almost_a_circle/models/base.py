dba34a9d5513d9244680f877c368a9c076d77a0e
othmandaha <dahaothman02@gmail.com>
2024-02-12 21:11:51 +0100

    base and rectangle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""base class definition"""
+
+
+class Base:
+    """Base class.
+
+    this class will be the base of all other classes in this project."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """New base initiation.
+
+        Args:
+            id (int): new base identity.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""Rectangle class definition."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle Class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the hight of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+                raise TypeError("height must be an integer")
+        if value <= 0:
+                raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+         """get the x of the rectangle"""
+         return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+                raise TypeError("x must be an integer")
+        if value < 0:
+                raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+         """get the y of the rectangle"""
+         return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+                raise TypeError("y must be an integer")
+        if value < 0:
+                raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def are(self):
+         """calculates the are of the rectangle."""
+         return self.height * self.width
+    
+    def display(self):
+         """Prints the rectangle using #."""
+         for i in range(self.height):
+            for j in range(self.width):
+                print("#", end='')
+            print("")
+
+    def __str__(self):
+         """Returns print() or str() representation."""
+         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                        self.x, self.y,
+                                                        self.width, self.height)
