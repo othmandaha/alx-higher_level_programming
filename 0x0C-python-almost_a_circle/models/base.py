8d4e0202260425b4fbde9c07ecede80b63c864ec
othmandaha <dahaothman02@gmail.com>
2024-02-12 23:34:30 +0100

    to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base class definition"""
+import json
 
 
 class Base:
@@ -20,3 +21,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return json formated string."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
