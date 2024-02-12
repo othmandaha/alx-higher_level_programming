d36768a3a745d6529d68eb53054ba93a175d6940
othmandaha <dahaothman02@gmail.com>
2024-02-12 23:21:31 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -141,6 +141,16 @@ class Rectangle(Base):
                 elif k == "y":
                     self.y = v
 
+    def to_dictionary(self):
+        """Returns the dictionary representation of the class."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
     def __str__(self):
         """Returns print() or str() representation."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
