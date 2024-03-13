dcf8a7f4917546f2747aeb0254a0c7c6db4037e1
othmandaha <dahaothman02@gmail.com>
2024-03-13 20:46:40 +0000

    edited

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- create new database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `state` (
     `id` INT NOT NULL DEFAULT 1 AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL,
