922f6beea9c796af64ce2f2e9736f7b580fda205
othmandaha <dahaothman02@gmail.com>
2024-03-13 19:52:20 +0000

    fixed

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creats new user and table with selecet privilege
 CREATE DATABASE IF NOT EXITS hbtn_0d_2;
 CREATE USER IF NOT EXITS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
