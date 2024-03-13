ee5fa6a4754e13987f629d8d33ae038708f21765
othmandaha <dahaothman02@gmail.com>
2024-03-13 20:29:16 +0000

    fixed

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates new table force_name
-CREATE TABLE IF NOT EXISTS `table_force` (`id` INT, `name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS table_force (id INT, name VARCHAR(256) NOT NULL);
