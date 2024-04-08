5838ddcf168288ea285c12b31119aaaf5675f645
othmandaha <dahaothman02@gmail.com>
2024-04-08 17:42:05 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const converted = parseInt(process.argv[2]);
-
 if (Number.isNaN(converted)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', converted);
+  console.log('My number:', converted);
 }
