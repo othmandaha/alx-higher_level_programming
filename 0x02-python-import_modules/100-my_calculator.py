714d855add9341db7f6be2ed07d74cee5fabc22c
othmandaha <dahaothman02@gmail.com>
2024-04-08 17:25:20 +0000

    concat

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,30 @@
-#!/usr/bin/python3
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-    valid_operators = ['+', '*', '/', '-']
 
-    if (len(sys.argv) - 1) != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
-    if op in valid_operators:
-        if op == '+':
-            print("{} {} {} = {}".format(a, op, b, add(a, b)))
-            sys.exit(0)
-        elif op == '-':
-            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-            sys.exit(0)
-        elif op == '*':
-            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-            sys.exit(0)
-        elif op == '/':
-            print("{} {} {} = {}".format(a, op, b, div(a, b)))
-            sys.exit(0)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+# Create the nodes
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+
+
+def height_of_binary_tree(node):
+    # Base case: if node is null, return -1
+    if node is None:
+        return -1
+    
+    # Recursively calculate the height of the left and right subtrees
+    left_height = height_of_binary_tree(node.left)
+    right_height = height_of_binary_tree(node.right)
+    
+    # Return the maximum height of the left and right subtrees, plus 1 for the current node
+    return max(left_height, right_height) + 1
+
+# Test the algorithm
+print("Height of the binary tree:", height_of_binary_tree(root))
