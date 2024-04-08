197bdfb9a1965d806b990e58c597e1da580ea545
othmandaha <dahaothman02@gmail.com>
2024-04-08 17:15:49 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length < 3) {
+let i = 0;
+
+while (args[i] !== undefined) {
+  i++;
+}
+
+if (i < 3) {
   console.log('No argument');
 } else {
   console.log(args[2]);
