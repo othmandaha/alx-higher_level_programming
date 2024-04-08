79ecb0239ebd307a0d2a8a327155bb89a58e4817
othmandaha <dahaothman02@gmail.com>
2024-04-08 23:29:35 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort((a, b) => a - b)[1];
+  const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
