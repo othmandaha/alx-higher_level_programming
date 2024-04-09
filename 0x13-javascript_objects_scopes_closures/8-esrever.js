07cbf5eea9efd35a85082ac62bbe23d11006591e
othmandaha <dahaothman02@gmail.com>
2024-04-09 17:37:34 +0000

    reverse

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const newList = [];
+  let size = list.length;
+  while (size >= 0) {
+    newList.push(list[size - 1]);
+    size--;
+  }
+  return newList;
+};
