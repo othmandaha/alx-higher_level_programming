f09a355443ebb67a1286963c1b0ef5538186a7bf
othmandaha <dahaothman02@gmail.com>
2024-04-11 21:12:10 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[2],
+        db=sys.argv[3],
         port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name From
