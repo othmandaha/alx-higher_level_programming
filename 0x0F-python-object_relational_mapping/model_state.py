664414b4dd2f6636d8477d8d4d1d30bae042bef0
othmandaha <dahaothman02@gmail.com>
2024-04-12 20:08:16 +0000

    6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ Contains State class and Base, an instance of declarative_base()
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = Metadata()
+mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 
@@ -14,6 +14,5 @@ class State(Base):
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
