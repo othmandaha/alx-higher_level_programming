937d3a34175e64adeaf5d743747e95b583ca00df
othmandaha <dahaothman02@gmail.com>
2024-04-12 21:26:13 +0000

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
 from modele_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
