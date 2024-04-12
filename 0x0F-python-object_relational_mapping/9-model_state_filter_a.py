761a9d7b0303a82691392e7b0898f9f4de51a23c
othmandaha <dahaothman02@gmail.com>
2024-04-12 22:39:33 +0000

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,9 +3,9 @@
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchmey.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
