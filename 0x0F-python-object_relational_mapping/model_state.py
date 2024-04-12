f39e6f377029a0b8fbd7f46f94fd4847167e5ed9
othmandaha <dahaothman02@gmail.com>
2024-04-12 20:02:36 +0000

    6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
-from sqlalchemy import Column, Integer, String, Metadata
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = Metadata()
