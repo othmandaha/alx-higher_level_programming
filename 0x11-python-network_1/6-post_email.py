0032bdcfaac71ea075de402e2ac9712e3f6c2a79
othmandaha <dahaothman02@gmail.com>
2024-04-26 22:33:43 +0100

    fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
+    request = urllib.request.Request(url, method='POST')
     with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
